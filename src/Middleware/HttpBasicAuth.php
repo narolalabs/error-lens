fb103165eb994bb44a2303ecbb367e3bd30e07b8
Narola Labs <narola.packages@narola.email>
2024-01-10 18:50:38 +0530

    Allow user to access the system if authentication username and password not set

--- src/Middleware/HttpBasicAuth.php
+++ src/Middleware/HttpBasicAuth.php
@@ -21,23 +21,25 @@ class HttpBasicAuth
         // Store configuration in cache
         if (Cache::get('authenticationDetails')) {
             $validCredentials = Cache::get('authenticationDetails');
-        }
-        else {
+        } else {
             // If configuration data is not in the cache, then pick from database
             $validCredentials = $this->configurationService->fetchConfigurationsDetail(['authenticate.username', 'authenticate.password']);
             Cache::put('authenticationDetails', $validCredentials, now()->addWeek(1));
         }
 
-        
-
         $user = $request->getUser();
         $password = $request->getPassword();
 
+        // Only authenticate if user set the username and password 
+        // using the command "php artisan error-lens:authentication"
         if (
-            !isset($validCredentials['authenticate.username']) ||
-            !isset($validCredentials['authenticate.password']) ||
-            $validCredentials['authenticate.username'] != $user ||
-            !Hash::check($password, $validCredentials['authenticate.password'])
+            (
+                isset($validCredentials['authenticate.username']) &&
+                isset($validCredentials['authenticate.password'])
+            ) && (
+                $validCredentials['authenticate.username'] != $user ||
+                !Hash::check($password, $validCredentials['authenticate.password'])
+            )
         ) {
             $headers = ['WWW-Authenticate' => 'Basic'];
             return response('Unauthorized', 401, $headers);
